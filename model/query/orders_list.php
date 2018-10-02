1e771cead94647c79486423f0118bf90de265a05
Cristopher Suárez <c.suarez.chacon@gmail.com>
2018-10-02 18:14:46 -0300

    se mejora consulta

--- model/query/orders_list.php
+++ model/query/orders_list.php
@@ -2,6 +2,8 @@
 	include '../conn.php';
 	include '../error.php';
 
+	$arr = array();
+
 	try {
 		$conn = new PDO("mysql:host=$serv;dbname=$dbnm", $user, $pass);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -10,16 +12,18 @@
 		$stmt = $conn->prepare($queryStr);
 		$stmt->execute();
 
-		foreach ($stmt->fetchAll() as $row) {
-			echo $row['orde_id'];
-			echo $row['orst_id'];
-			echo $row['cust_id'];
-			echo "------------------";
+		foreach ($stmt->fetchAll() as $row => $value) {
+			$orderRow = (object) array(
+				'orde_id' => $value["orde_id"],
+				'orst_id' => $value["orst_id"],
+				'cust_id' => $value["cust_id"]);
+
+			array_push($arr, $orderRow); 		
 		}
 
 	} catch (PDOException $e) {
 		echo "error";
 	}
-	echo "ingresado";
+	//echo $arr[0]->orst_id;//así se puede obtener la variable correspondiente al registro
 	$conn = null;
 ?>
