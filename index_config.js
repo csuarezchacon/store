cf31ef185944ebaf85411f805a83aa3cc8c197b0
Cristopher Suárez <c.suarez.chacon@gmail.com>
2018-10-08 00:10:48 -0300

    Algunos avances

--- index_config.js
+++ index_config.js
@@ -1,11 +1,14 @@
 (function (){
 	angular.module('index-module').config(
 	['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-		$urlRouterProvider.otherwise('/store');
+		$urlRouterProvider.otherwise('store');
 
-		$stateProvider.state('/store', {
+		$stateProvider.state('store', {
 			url: '/store',
 			templateUrl: 'w3/pages/store/store.html'
+		}).state('cart', {
+			url: '/cart',
+			templateUrl: 'w3/directive/cart/cart.html'
 		});
   }]);
 })();
